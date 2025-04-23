eeac89fc3060846039505a0e9263a258e7bf1c8d
hilda <136410020+hildanih@users.noreply.github.com>
2025-04-23 21:32:53 +0800

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,1 +1,66 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe{
+    uint256 public constant MINIMUM_USD = 5;
+    using PriceConverter for uint256;
+
+    address[] public funders;
+
+    mapping (address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value > MINIMUM_USD, "you didn't send enough eth");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        //for loop
+        uint256 funderIndex;
+        for (funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Call failed");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
+
+//// Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
